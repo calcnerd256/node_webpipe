f6291be05dd265c3d6d10390d59f2d718cea619f
Montana Rowe <calcnerd256@gmail.com>
2012-11-21 16:55:53 -0600

    removed vestigial fluentCall function

--- serve.js
+++ serve.js
@@ -11,45 +11,6 @@ server.listen(port,  afterServerSetup);
 //now that you have an HTTP server listening on a port, the program won't end
 //if you need to end the program, you can press Ctrl+C at the command line
 
-function fluentCall(object, key, parameters){
- //sometimes, we want to call a method on an object and then call another one
- // as in, someObject.someMethod(x, y, z); someObject.anothermethod(w);
- //sometimes, the object we want to use is the result of some expression
- //we can say var ob = someExpression; ob.someMethod(x,y,z); ob.anothermethod(w);
- //but it's nice to be able to say (someExpression).someMethod(x,y,z).anothermethod(w)
- // this is called "fluent" style (I don't know who coined that term)
- //some methods work like that, returning the object that called them
- //however, there's nothing that guarantees that any given method works like that
- //so here's a function that lets you act like a method does that, whether it does or not
-
- //methods in JS are just properties
- //I can have an object x that has a property like x.color
- //but if I have a method x.toString() , that's really just a function-valued property
- //we can even patch methods into existing objects by storing functions in their properties
- //properties can be accessed with a . or with []
- //if you write x.y , it means the same thing as x["y"]
- //[] lookup takes a string
- //it's like reifying the name of the property
- var method = object[key];
- //so if key == "someMethod"
- // then object["someMethod"] is the same thing as object.someMethod
- //you can even use property names that would cause syntax errors if you tried to use . lookup on them!
- // any string works
-
- //every function has an apply method
- //there's a special variable in JS called this
- //when you call someObject.someMethod(someArg, anotherArg, etc)
- // any reference to the "this" variable in that method body equals someObject
- //sometimes you want to act like you called a method with a different this
- //apply takes the this as its first parameter
- //its second parameter is a list of the arguments you want to pass to the method
- //JS doesn't care how many arguments you pass to functions
- method.apply(object, parameters);
-
- //now, to make the helper act like a fluent call
- return object;
-}
-
 //respond is a function that takes an HTTP request and an HTTP response
 //when a request comes in, the http library fires an event
 //the createServer function binds whatever you pass to it to that event
@@ -76,8 +37,7 @@ function respond(request, response){
 
 function handleGet(request, response){
  //before, this behavior was implemented as a one-liner
- //but that makes it hard to read, so I'm rewriting it
- //which means my only use of fluentCall is no longer necessary
+ //but that makes it hard to read, so I've rewritten it as a function
 
  //HTTP status code 200 means "OK"
  var statusCode = 200;
