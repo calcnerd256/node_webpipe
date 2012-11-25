dbc35153f0a9894df4ae7271582f947d36fe74bb
Montana Rowe <calcnerd256@gmail.com>
2012-11-25 16:22:59 -0600

    indentation

--- streams.js
+++ streams.js
@@ -30,49 +30,51 @@ EventSponge.prototype.resume = function resume(){
 };
 
 
- function SingleCharacterDelimiterLexerEmitter(stream, delimiter){
-  this.delimiter = delimiter;
-  this.emitter = new EventSponge();
-  stream.on(
-   "data",
-   this.handleChunk.bind(this)
-  ).on(
-   "end",
-   function(){
-    this.delimit();
-    this.emitter.emit("end");
-   }.bind(this)
-  );
-  this.delimit();
- }
- SingleCharacterDelimiterLexerEmitter.prototype.delimit = function delimit(){
-  if(this.buffer)
-   this.buffer.emit.bind(this.buffer, "end").apply(this.buffer, arguments);
+function SingleCharacterDelimiterLexerEmitter(stream, delimiter){
+ this.delimiter = delimiter;
+ this.emitter = new EventSponge();
+ stream.on(
+  "data",
+  this.handleChunk.bind(this)
+ ).on(
+  "end",
+  function(){
+   this.delimit(true);
+   this.emitter.emit("end");
+  }.bind(this)
+ );
+ this.delimit();
+}
+SingleCharacterDelimiterLexerEmitter.prototype.delimit = function delimit(last){
+ if(this.buffer)
+  this.buffer.emit.bind(this.buffer, "end").apply(this.buffer, arguments);
+ delete this.buffer;
+ if(!last)
   this.emitter.emit("lexer", this.buffer = new EventSponge());
- };
- SingleCharacterDelimiterLexerEmitter.prototype.handleChunk = function handleChunk(chunk){
-  if(chunk.toString().indexOf(this.delimiter) == -1)
-   return this.buffer.emit("data", chunk);
-  var tokens = chunk.toString().split(this.delimiter);//not binary-safe
-  var remaining = tokens.pop();
-  remaining.map(
-   function(token){
-    this.buffer.emit("data", token);
-    this.delimit();
-   }
-  );
-  this.buffer.emit("data", remaining);
- };
- SingleCharacterDelimiterLexerEmitter.prototype.on = function on(){
-  this.emitter.on.apply(this.emitter, arguments);
-  return this;
- };
- SingleCharacterDelimiterLexerEmitter.prototype.pause = function pause(){
-  return applyFrom(this.emitter, "pause", arguments);
- };
- SingleCharacterDelimiterLexerEmitter.prototype.resume = function resume(){
-  return applyFrom(this.emitter, "resume", arguments);
- };
+};
+SingleCharacterDelimiterLexerEmitter.prototype.handleChunk = function handleChunk(chunk){
+ if(chunk.toString().indexOf(this.delimiter) == -1)
+  return this.buffer.emit("data", chunk);
+ var tokens = chunk.toString().split(this.delimiter);//not binary-safe
+ var remaining = tokens.pop();
+ remaining.map(
+  function(token){
+   this.buffer.emit("data", token);
+   this.delimit();
+  }
+ );
+ this.buffer.emit("data", remaining);
+};
+SingleCharacterDelimiterLexerEmitter.prototype.on = function on(){
+ this.emitter.on.apply(this.emitter, arguments);
+ return this;
+};
+SingleCharacterDelimiterLexerEmitter.prototype.pause = function pause(){
+ return applyFrom(this.emitter, "pause", arguments);
+};
+SingleCharacterDelimiterLexerEmitter.prototype.resume = function resume(){
+ return applyFrom(this.emitter, "resume", arguments);
+};
 
 
 function applyFrom(that, methodName, args){
