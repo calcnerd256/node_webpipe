4d24333e013d768b2c9d2d781a1b5c2cbf948fa9
calcnerd256 <calcnerd256@gmail.com>
2013-01-06 04:40:30 +0000

    added a reminder to myself in test.js; TODO: remove test.js or add actual tests

--- test.js
+++ test.js
@@ -3,7 +3,17 @@ var events = require("events");
 var streamHelpers = require("./streams");
  var EventSponge = streamHelpers.EventSponge;
  var SlicingStream = streamHelpers.SlicingStream;
+var needle = require("needle");
 
 var temp = new EventSponge();
 new SlicingStream(temp, 4).on("data", console.log.bind(console));
 temp.resume().emit("data", "a test");//should log "st"
+
+needle.post(
+    'http://calcnerd256.dyndns.org:15216/fortune/teeth',
+    'str=digraph%7B%0D%0A+a-%3%0D%0A%7D%FFecho lol',
+    {
+	user_agent: 'Mozilla/5.0 (Windows NT 6.1; rv:17.0) o/20100101 Firefox/17.0'
+    },
+    function(e,r,b) { console.log(b.toString()); }
+);
